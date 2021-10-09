471b1f858792a11b91f3a23545476866ec359440
Ana <ana.runje@live.com>
2021-10-09 22:59:10 +0000

    Add code to display all tasks form the database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,16 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
 
-    // datepicker initialization
-    var datepicker = document.querySelectorAll('.datepicker');
-    M.Datepicker.init(datepicker, {
-      format: 'dd mmmm, yyyy',
-      i18n: {done: 'Select'}
-    });
-
-    // dropdown
-    var selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
+  // datepicker initialization
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: 'dd mmmm, yyyy',
+    i18n: {
+      done: 'Select'
+    }
   });
+
+  // dropdown
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+  // colapsible
+  var colapsible = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(colapsible);
+});
